06c11b0fd307eb707bafcd9806e80dcb0342f7de
David Ayllón Martín <david.ayllon1@educa.madrid.org>
2023-12-15 12:35:17 +0100

    P681

---
+++ src/P681_LaEscaleraDeAlba.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P681_LaEscaleraDeAlba {
+    static Scanner in;
+
+    public static boolean casoDePrueba(){
+        if (!in.hasNext()){
+            return false;
+        }else {
+            int numeroColores = in.nextInt();
+            int[] piezas = new int[numeroColores];
+            for (int i = 0; i < numeroColores; i++) {
+                piezas[i] = in.nextInt();
+            }
+            Arrays.sort(piezas);
+            int suma = 0;
+            int maxPiezasPosibles = piezas[piezas.length-1];
+            for (int i = numeroColores - 1; i >= 0; i--) {
+                int piezasNivel = Math.min(piezas[i], maxPiezasPosibles);
+                suma += piezasNivel;
+                maxPiezasPosibles = piezasNivel - 1;
+                if (maxPiezasPosibles < 1){
+                    break;
+                }
+            }
+            System.out.println(suma);
+            return true;
+        }
+    }
+
+    public static void main(String[] args){
+        in = new Scanner(System.in);
+
+        while (casoDePrueba());
+    }
+}
+
+
